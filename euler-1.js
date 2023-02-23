6adf94232ec9264f778fecd2a4c45a51f4200a37
scooch-gg <max.nuckolls@gmail.com>
2023-02-23 16:10:25 -0600

    Create euler-1.js

---
+++ euler-1.js
@@ -0,0 +1,13 @@
+// Multiples of 3 or 5
+
+println(sumMultiples(1000));
+
+function sumMultiples(num) {
+    let sum = 0;
+    for (let i = 0; i < num; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
